a3ae5c8f5426b1e40e61171fca4e1b2ccde465a7
Sebastian Marcelo Suarez <sebastian@Sebastians-MacBook-Pro.local>
2023-08-16 22:05:57 -0300

    Cleaning

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,209 +1,71 @@
 package com.example.demo;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch.core.BulkRequest;
-import co.elastic.clients.elasticsearch.core.BulkResponse;
-import co.elastic.clients.elasticsearch.core.SearchTemplateResponse;
-import co.elastic.clients.elasticsearch.core.bulk.BulkResponseItem;
-import co.elastic.clients.elasticsearch.core.search.Hit;
-import co.elastic.clients.json.JsonData;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
+
+import com.example.demo.request.SumRequest;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 @RestController
 public class Controller {
 
+
+    /**
+     * Debes desarrollar una API REST en Spring Boot utilizando java 11 o superior, con las siguientes funcionalidades:
+     *
+     * 1) Debe contener un servicio llamado por api-rest que reciba 2 números, los sume, y le aplique una suba de un porcentaje que
+     * debe ser adquirido de un servicio externo (por ejemplo, si el servicio recibe 5 y 5 como valores, y el porcentaje devuelto por el servicio externo es 10,
+     * entonces (5 + 5) + 10% = 11). Se deben tener en cuenta las siguientes consideraciones:
+     *
+     *      El servicio externo puede ser un mock, tiene que devolver el % sumado.
+     *
+     *      Dado que ese % varía poco, podemos considerar que el valor que devuelve ese servicio no va cambiar por 30 minutos.
+     *
+     *      Si el servicio externo falla, se debe devolver el último valor retornado. Si no hay valor, debe retornar un error la api.
+     *
+     *      Si el servicio falla, se puede reintentar hasta 3 veces.
+     *
+     * 2) Historial de todos los llamados a todos los endpoint junto con la respuesta en caso de haber sido exitoso. Responder en Json, con data paginada. El guardado del historial de llamadas no debe sumar tiempo al servicio invocado, y en caso de falla, no debe impactar el llamado al servicio principal.
+     *
+     * 3) La api soporta recibir como máximo 3 rpm (request / minuto), en caso de superar ese umbral, debe retornar un error con el código http y mensaje adecuado.
+     *
+     * 4) El historial se debe almacenar en una database PostgreSQL.
+     *
+     * 5) Incluir errores http. Mensajes y descripciones para la serie 4XX.
+     *
+     *
+     */
+
     @CrossOrigin
-    @GetMapping("/indexmovies/title/{movie_title}")
-    private ResponseEntity<String> index_movie(@PathVariable("movie_title") String movieTitle) {
-
-        final String uri = String.format("https://jsonmock.hackerrank.com/api/moviesdata/search/?Title=%s&page=1",movieTitle);
-
-        RestTemplate restTemplate = new RestTemplate();
-        String restResult = restTemplate.getForObject(uri, String.class);
-        JSONObject root = new JSONObject(restResult);
-
-        JSONArray data = root.getJSONArray("data");
-        int totalPages = root.getInt("total_pages");
-        int page = root.getInt("page");
-
-        System.out.println(movieTitle);
-
-        ArrayList<Movie> myMovies = new ArrayList<>();
-
-        data.forEach(element -> {
-            myMovies.add(new Movie(
-                    (Integer) ((JSONObject) element).get("Year"),
-                    (String) ((JSONObject) element).get("imdbID"),
-                    (String) ((JSONObject) element).get("Title")));
-        });
-        while (page < totalPages) {
-            page++;
-            String uriNextPage = String.format("https://jsonmock.hackerrank.com/api/moviesdata/search/?Title=%s&page=%s",movieTitle, page);
-            JSONArray newData = getData(uriNextPage);
-            newData.forEach(element -> {
-                myMovies.add(new Movie(
-                        (Integer) ((JSONObject) element).get("Year"),
-                        (String) ((JSONObject) element).get("imdbID"),
-                        (String) ((JSONObject) element).get("Title")));
-            });
-
-        }
-
-        // index documents
-        // Create the low-level client
-        RestClient restClient = RestClient.builder(
-                new HttpHost("localhost", 9200)).build();
-        // Create the transport with a Jackson mapper
-        ElasticsearchTransport transport = new RestClientTransport(
-                restClient,new JacksonJsonpMapper());
-        // And create the API client
-        ElasticsearchClient client = new ElasticsearchClient(transport);
-
-        BulkRequest.Builder br = new BulkRequest.Builder();
-
-        for (Movie movie : myMovies) {
-            br.operations(op -> op
-                    .index(idx -> idx
-                            .index("movies")
-                            .id(movie.getImdbID())
-                            .document(movie)
-                    )
-            );
-        }
-
-        try {
-
-            BulkResponse result = client.bulk(br.build());
-
-            if (result.errors()) {
-                System.out.println("Bulk had errors");
-                for (BulkResponseItem item: result.items()) {
-                    if (item.error() != null) {
-                        System.out.println(item.error().reason());
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            System.out.println("error" +  e.getMessage());
-        }
-
-
-        return new ResponseEntity<String>("Index Created",HttpStatus.OK);
-
-
-    }
-
-    // EJEMPLO LLAMADA LOCAL http://localhost:8080/searchmovie/title/The
-    @CrossOrigin
-    @GetMapping("/searchmovie/title/{movie_title}")
-    private ResponseEntity<List<Movie>>search_movie(@PathVariable("movie_title") String movieTitle) {
-
-        // index documents
-        // Create the low-level client
-        RestClient restClient = RestClient.builder(
-                new HttpHost("localhost", 9200)).build();
-        // Create the transport with a Jackson mapper
-        ElasticsearchTransport transport = new RestClientTransport(
-                restClient,new JacksonJsonpMapper());
-        // And create the API client
-        ElasticsearchClient client = new ElasticsearchClient(transport);
-
+    @GetMapping("/calculator/sum")
+    private ResponseEntity<Integer>suma(@RequestBody SumRequest movieTitle) {
         try{
-            client.putScript(r -> r
-                    .id("searchByTitle")
-                    .script(s -> s
-                            .lang("mustache")
-                            .source("{\"size\":1000,\"query\":{\"match\":{\"{{field}}\":\"{{value}}\"}}}")
-                    ));
-
-            SearchTemplateResponse<JsonData> searchResponse = client.searchTemplate(r -> r
-                            .index("movies")
-                            .id("searchByTitle")
-                            .params("field", JsonData.of("title"))
-                            .params("value", JsonData.of(movieTitle)),
-                    JsonData.class
-            );
-
-            List<Movie> moviesReturn = new ArrayList<>();
-            for (Hit<JsonData> hit: searchResponse.hits().hits()) {
-                JSONObject movieJson = new JSONObject(hit.source().toString());
-                new JSONObject(hit.source().toString()).get("title");
-                moviesReturn.add(new Movie(Integer.parseInt(movieJson.get("year").toString()),
-                        (movieJson.get("imdbID").toString()),
-                        (movieJson.get("title").toString())));
-
-            }
-            return new ResponseEntity<List<Movie>>(moviesReturn, HttpStatus.OK);
+            Integer porcentualResult = calculatePercentageValue(movieTitle);
+            return new ResponseEntity<Integer>(porcentualResult, HttpStatus.OK);
         } catch (Exception e) {
             System.out.println("error" +  e.getMessage());
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
-    private JSONArray getData(String uri) {
+    private Integer calculatePercentageValue(SumRequest sumRequest) {
 
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-        JSONObject root = new JSONObject(result);
-
-        return root.getJSONArray("data");
-
+        Integer percentage = getPercentageMocked();
+        Integer sumados =  Integer.sum(sumRequest.getNumberOne(), sumRequest.getNumberTwo());
+        return sumados + ( (sumados * percentage) / 100);
     }
 
-    ;
-
-    public class Movie {
-        Integer Year;
-        String imdbID;
-        String Title;
-
-        public Movie(Integer year, String imdbID, String title) {
-            Year = year;
-            this.imdbID = imdbID;
-            Title = title;
-        }
-
-        public Integer getYear() {
-            return Year;
-        }
-
-        public void setYear(Integer year) {
-            Year = year;
-        }
-
-        public String getImdbID() {
-            return imdbID;
-        }
-
-        public void setImdbID(String imdbID) {
-            this.imdbID = imdbID;
-        }
-
-        public String getTitle() {
-            return Title;
-        }
-
-        public void setTitle(String title) {
-            Title = title;
-        }
+    private Integer getPercentageMocked() {
+        // Mocked system, it may use a retrier ...
+       return ThreadLocalRandom.current().nextInt(1, 100);
     }
 
 }
---
+++ src/main/java/com/example/demo/request/SumRequest.java
@@ -0,0 +1,23 @@
+package com.example.demo.request;
+
+public class SumRequest {
+
+    Integer numberOne;
+    Integer numberTwo;
+
+    public Integer getNumberOne() {
+        return numberOne;
+    }
+
+    public void setNumberOne(Integer numberOne) {
+        this.numberOne = numberOne;
+    }
+
+    public Integer getNumberTwo() {
+        return numberTwo;
+    }
+
+    public void setNumberTwo(Integer numberTwo) {
+        this.numberTwo = numberTwo;
+    }
+}
